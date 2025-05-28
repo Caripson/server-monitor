a6649bea0e13469d06e70e30c802581f83573493
Johan Caripson <31686838+Caripson@users.noreply.github.com>
2025-05-28 13:04:17 +0200

    Create agent.py

---
+++ agent.py
@@ -0,0 +1,34 @@
+import requests
+import time
+import yaml
+import subprocess
+
+INFLUX_URL = "http://localhost:8086/api/v2/write?org=server-monitor&bucket=monitoring&precision=s"
+TOKEN = "mytoken"
+
+with open("endpoints.yml") as f:
+    config = yaml.safe_load(f)
+
+for e in config["endpoints"]:
+    name = e["name"]
+    url = e["url"]
+    typ = e.get("type", "http")
+    timestamp = int(time.time())
+    if typ == "http":
+        try:
+            r = requests.get(url, timeout=5)
+            t_nl = r.elapsed.total_seconds()
+            line = f'monitor,name={name},type=http time_total={t_nl} {timestamp}'
+        except Exception as ex:
+            line = f'monitor,name={name},type=http time_total=0 {timestamp}'
+    elif typ == "ping":
+        try:
+            output = subprocess.check_output(f'ping -c 1 -w 1 {url}', shell=True).decode()
+            ms = float(output.split("time=")[-1].split(" ")[0])
+            line = f'monitor,name={name},type=ping ping_time={ms} {timestamp}'
+        except Exception:
+            line = f'monitor,name={name},type=ping ping_time=0 {timestamp}'
+    else:
+        continue
+    headers = {"Authorization": f"Token {TOKEN}"}
+    requests.post(INFLUX_URL, data=line, headers=headers)
